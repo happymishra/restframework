cccd7ba792e4b9ca0fd66359f8c103d3b12333c3
Rupesh Mishra <rupesh.mishra@visiblealpha.com>
2019-06-22 19:51:00 +0530

    Initial commit for django rest framework

---
+++ blogs/article/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import ArticleView
+
+urlpatterns = [
+    path('articles/', ArticleView.as_view()),
+    path('articles/<int:pk>', ArticleView.as_view())
+]
